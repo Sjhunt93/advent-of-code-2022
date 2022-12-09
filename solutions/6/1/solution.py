40cd998d77f5c7be8907237d4638f2000df89344
hackerman <sam@aflorithmic.ai>
2022-12-09 21:45:51 +0000

    refactored solution 6 to use a sigle regex

--- solutions/6/1/solution.py
+++ solutions/6/1/solution.py
@@ -1,11 +1,13 @@
+import re
 
 with open("input.txt") as f:
-    codes = f.read()
-    print(codes)
-    #codes = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
-    for i in range(0, len(codes)-3):
-        sub = codes[i:i+4]
-        print(sub)
-        if len(set(sub)) == 4:
-            print(i+4)
-            break
+    print(re.search(r"(\w)(?!\1)(\w)(?!\1|\2)(\w)(?!\1|\2|\3)(\w)(?!\1|\2|\3|\4)", f.read()).end())
+
+    # print(codes)
+    # #codes = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    # for i in range(0, len(codes)-3):
+    #     sub = codes[i:i+4]
+    #     print(sub)
+    #     if len(set(sub)) == 4:
+    #         print(i+4)
+    #         break
