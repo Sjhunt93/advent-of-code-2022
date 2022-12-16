544cb3f76f2dae2216bef0c9effd6c08f4557a68
hackerman <sam@aflorithmic.ai>
2022-12-16 21:22:41 +0000

    day 13 done

--- solutions/13/2/solution.py
+++ solutions/13/2/solution.py
@@ -1,28 +1,5 @@
 
 import itertools
-
-
-indexes_in_order = []
-
-def compare_lists(l1, l2):
-    if l1 == None:
-        return True
-    if l2 == None:
-        return False
-
-    if not isinstance(l1, list):
-        l1 = [l1]
-    if not isinstance(l2, list):
-        l2 = [l2]
-    for a,b in itertools.zip_longest(l1, l2):
-        if isinstance(a, int) and isinstance(b, int):
-            if a < b:
-                return True
-            elif a > b:
-                return False
-        else:
-            return compare_lists(a, b)
-
     
 
 def expand_items(l1, l2):
@@ -40,6 +17,7 @@ def expand_items(l1, l2):
                 return "order"
             elif a > b:
                 return "not"
+
         elif isinstance(a, int) and isinstance(b, list):
             r = expand_items([a],b)
             if r != "continue":
@@ -49,27 +27,44 @@ def expand_items(l1, l2):
             if r != "continue":
                 return r
         elif isinstance(a, list) and isinstance(b, list):
-            #if a and b:
-                r = expand_items(a,b)
-                if r != "continue":
-                    return r
+            r = expand_items(a,b)
+            if r != "continue":
+                return r
+
     return "continue"
 
 
-#with open("test.txt") as f:
+to_sort = []
 with open("input.txt") as f:
+#with open("test.txt") as f:
 
     lines = f.read()
     packets = [i.split("\n") for i in lines.split("\n\n")]
         
     for i, p in enumerate(packets):
-        l1 = eval(p[0])
-        l2 = eval(p[1])
-        print(l1)
-        print(l2)
-        if expand_items(l1, l2) == "order":
-            indexes_in_order.append(i+1)
+        to_sort.append(eval(p[0]))
+        to_sort.append(eval(p[1]))
         
-indexes_in_order.sort()
-print(indexes_in_order)
-print(sum(set(indexes_in_order)))
+
+sorted = [to_sort.pop()]
+to_sort += [[[2]], [[6]]]
+
+for i in range(0, len(to_sort)):
+    packet = to_sort[i]
+    for s in range(0, len(sorted)):
+        if expand_items(packet, sorted[s]) == "order":
+            sorted.insert(s, packet)
+            break
+        elif s == len(sorted)-1:
+            sorted.append(packet)
+    
+
+# debug
+print("\n\n\n")
+for i, s in enumerate(sorted[1:]):    
+    print(i, "~", expand_items(sorted[i-1], s))
+
+
+ia = sorted.index([[2]])+1
+ib = sorted.index([[6]])+1
+print(ia * ib)
---
+++ solutions/13/notes.md
@@ -0,0 +1,3 @@
+
+solution 1 = 5580
+solution 2 = 26200
